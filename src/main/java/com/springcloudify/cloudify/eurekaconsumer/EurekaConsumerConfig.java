a8aa290d6acbc3d1cc764e96168a442915fec3ea
王佳 <wangjia_1919@163.com>
2019-05-10 11:47:40 +0800

    add getaway

--- src/main/java/com/springcloudify/cloudify/eurekaconsumer/EurekaConsumerConfig.java
+++ src/main/java/com/springcloudify/cloudify/eurekaconsumer/EurekaConsumerConfig.java
@@ -4,8 +4,10 @@ import com.springcloudify.cloudify.eurekaconsumer.beans.RibbonConfiguration;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
+import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * Create by Jarvis.wang on Jarvis's device
